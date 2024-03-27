f45a7968d7f9d5e1bc75ff3b70b9c19f507eeb11
RADBRAD1 <112186951+RADBRAD1@users.noreply.github.com>
2024-03-27 11:45:45 -0400

    v1.000018

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -5,7 +5,7 @@ import TypeSection from "./TypeSection";
 import FacilitiesSection from "./FacilitiesSection";
 import GuestsSection from "./GuestsSection";
 import ImagesSection from "./ImagesSection";
-import { HotelType } from "../../../../backend/src/shared/types";
+import { HotelType } from "../../../../Backend/src/shared/types";
 import { useEffect } from "react";
 
 
